6c67b245b9e58e9d6a93db30d640c851d09da766
failququ <trashplayer@mail.ru>
2020-12-24 10:23:04 +0300

    BubbleSort.java file has been modified

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,48 +6,49 @@ public class BubbleSort {
         Sort first = new Sort();
     }
 
-    public static class Sort{
+    public static class Sort {
 
-        public Sort(){
-            List<Integer> Data =inputData();
+        public Sort() {
+            List<Integer> Data = inputData();
             System.out.println(Data);
             Integer[] ArrData = new Integer[Data.size()];
-            for (int i=0;i<Data.size();i++){
-                ArrData[i]=Data.get(i);
+            for (int i = 0; i < Data.size(); i++) {
+                ArrData[i] = Data.get(i);
             }
-            Integer[] result =Sorted(ArrData);
+            Integer[] result = Sorted(ArrData);
             System.out.println(Arrays.toString(result));
         }
 
 
-        public List inputData(){
+        public List inputData() {
             boolean stop = true;
-            List<Integer>  data = new ArrayList<Integer>();
-            while (stop == true){
+            List<Integer> data = new ArrayList<Integer>();
+            while (stop == true) {
                 Scanner nc = new Scanner(System.in);
-                try{
-                int a = nc.nextInt();
-                data.add(a);
-                }
-                catch (NumberFormatException e){
-                    stop=false;
-            }
-                catch (InputMismatchException e){
-                    stop=false;
+                try {
+                    int a = nc.nextInt();
+                    data.add(a);
+                } catch (NumberFormatException e) {
+                    stop = false;
+                } catch (InputMismatchException e) {
+                    stop = false;
                 }
             }
             return data;
         }
 
-        public Integer[] Sorted(Integer[] data1){
+        public Integer[] Sorted(Integer[] data1) {
             Integer[] data = data1;
             int n = data.length;
-            for(int i=0;i<n-1;i++){
-                for(int j=0;j<n-i-1;j++){
-                    if(data[j+1]>data[j]) {
-                        int tmp = data[j];
-                        data[j] = data[j + 1];
-                        data[j + 1] = tmp;
+            boolean isSorted = false;
+            while (!isSorted) {
+                isSorted = true;
+                for (int i = 1; i < n; i++) {
+                    if (data[i] < data[i - 1]) {
+                        int tmp = data[i];
+                        data[i] = data[i - 1];
+                        data[i - 1] = tmp;
+                        isSorted = false;
                     }
                 }
             }
